8baf0d992a84e05dc542964cf589c1587cb8fac0
zhyunk <gimwlgus@gmail.com>
2023-08-09 05:04:55 +0900

    Hash 자료구조 사용 위한 RedisTemplate 설정 수정
    1. 
    Hash 자료구조 사용 시 Dto 클래스를 사용하기 위해 
    <byte[], byte[]>로 작성되어있던 타입을 <byte[], Object> 로 수정해 주었다.
    
    
    2. 
    setHashValueSerializer() 의 인자를
    StringRedisSerializer 에서 GenericJackson2JsonRedisSerializer로 변경.

--- src/main/java/kim/zhyun/studyredis/config/RedisConfiguration.java
+++ src/main/java/kim/zhyun/studyredis/config/RedisConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -32,12 +33,12 @@ public class RedisConfiguration {
 
     @Bean
     public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        RedisTemplate<byte[], Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return redisTemplate;
     }

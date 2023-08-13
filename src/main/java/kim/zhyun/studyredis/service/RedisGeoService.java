9f91a206403b4f9af581d3371a30782c99f465ef
zhyunk <gimwlgus@gmail.com>
2023-08-14 08:43:54 +0900

    리팩토링 : 메서드 추출

--- src/main/java/kim/zhyun/studyredis/service/RedisGeoService.java
+++ src/main/java/kim/zhyun/studyredis/service/RedisGeoService.java
@@ -26,20 +26,16 @@ public class RedisGeoService {
     private final ObjectMapper objectMapper;
 
     private final String KEY = "STORE";
-    /**
-     * geospecial
-     */
-    public void geospecial() {
-        List<StoreDto> list = List.of(
-                StoreDto.of(1L, "a", 126.290992371209, 34.5121745149379, "1224"),
-                StoreDto.of(2L, "b", 127.041688751305, 37.5481104648655, "1244"),
-                StoreDto.of(3L, "c", 127.028065727387, 37.4986232309429, "1634"),
-                StoreDto.of(4L, "d", 129.300837210054, 35.5507005286619, "1734"),
-                StoreDto.of(5L, "e", 127.086439879278, 37.622218884578, "1284"),
-                StoreDto.of(6L, "f", 127.111270068545, 37.7337789487014, "1034"),
-                StoreDto.of(7L, "g", 127.044608514359, 36.4804589952686, "1934")
-        );
 
+    public void geospecial() {
+        List<StoreDto> list = getData();
+
+        save(list);
+
+        search();
+    }
+
+    private void save(List<StoreDto> list) {
         redisTemplate.delete(KEY);
 
         Long added = redisTemplate.opsForGeo().add(
@@ -49,7 +45,9 @@ public class RedisGeoService {
                     .collect(Collectors.toList())
         );
         log.info("added {}", added);
+    }
 
+    private void search() {
         redisTemplate.opsForGeo().search(
                 KEY,
                 GeoReference.fromCoordinate(new Point(127.047365178682, 37.5471882244091)),
@@ -59,15 +57,26 @@ public class RedisGeoService {
                         .includeCoordinates()
                         .includeDistance()
         ).forEach(result -> {
-                log.info(
-                        "🗺️ [{}] name : {} , Point : {}, Distance : {}",
-                        KEY,
-                        getDtoFromJson(result).name(),
-                        result.getContent().getPoint(),
-                        result.getDistance()
-                );
+            log.info(
+                    "🗺️ [{}] name : {} , Point : {}, Distance : {}",
+                    KEY,
+                    getDtoFromJson(result).name(),
+                    result.getContent().getPoint(),
+                    result.getDistance()
+            );
         });
+    }
 
+    private static List<StoreDto> getData() {
+        return List.of(
+                StoreDto.of(1L, "a", 126.290992371209, 34.5121745149379, "1224"),
+                StoreDto.of(2L, "b", 127.041688751305, 37.5481104648655, "1244"),
+                StoreDto.of(3L, "c", 127.028065727387, 37.4986232309429, "1634"),
+                StoreDto.of(4L, "d", 129.300837210054, 35.5507005286619, "1734"),
+                StoreDto.of(5L, "e", 127.086439879278, 37.622218884578, "1284"),
+                StoreDto.of(6L, "f", 127.111270068545, 37.7337789487014, "1034"),
+                StoreDto.of(7L, "g", 127.044608514359, 36.4804589952686, "1934")
+        );
     }
 
     private String getJsonFromDto(StoreDto dto) {
